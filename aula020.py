bc6754f52eaa24caa53e79b2cf836f029e4c748f
ArthurHSAngelo <arthurhenrique302007@gmail.com>
2025-06-21 18:11:38 -0300

    aula020 - exercício if e comparação

--- aula020.py
+++ aula020.py
@@ -2,6 +2,6 @@ primeiro_valor = input('Digite um valor: ')
 segundo_valor = input('Digite outro valor: ')
 
 if primeiro_valor >= segundo_valor:
-    print(f'O {primeiro_valor=} é maior ou igual que o {segundo_valor=}')
+    print(f'O {primeiro_valor=} é maior ou igual o {segundo_valor=}')
 else:
-    print(f'O {segundo_valor=} é maior que o {primeiro_valor=}')
+    print(f'O {segundo_valor=} é maior do que o {primeiro_valor=}')
