7c401d43c01e1195989c60c452eb83c9cc3c594e
ArthurHSAngelo <arthurhenrique302007@gmail.com>
2025-10-26 19:12:05 -0300

    aula036

--- aula036-atribuicao_com_operadores_aritmeticos.py
+++ aula036-atribuicao_com_operadores_aritmeticos.py
@@ -1,6 +1,13 @@
 """
 Operadores de atribuição
-= += -= *= /= //= **= %=
+= igual
++= mais igual
+-= menos igual
+*= multiplica igual
+/= divisão igual
+//= divisão inteira igual 
+**= potenciação igual
+%= modulo igual
 """
 contador = 10
 
